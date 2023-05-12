d2e348eb51c773739324014607d3c2c20665a23c
spikeiverson <112721775+spikeiverson@users.noreply.github.com>
2023-05-12 16:31:47 -0700

    Add files via upload

--- ShopInterface.py
+++ ShopInterface.py
@@ -3,12 +3,13 @@ from Purchasables import *
 from Player_and_Shop_Classes import *
 
 #WINDOW DIMENSIONS
-WIDTH = 1200
-HEIGHT = 800
+WIDTH = 1050
+HEIGHT = 700
 #NEWS = pygame.Rect((HEIGHT + 10), 30, (WIDTH - HEIGHT- 30), (HEIGHT / 3 - 15))
 INVENTORY = pygame.Rect((HEIGHT + 10), (HEIGHT / 3 + 30), (WIDTH - HEIGHT - 30), (HEIGHT / 3 - 15))
 SHOP = pygame.Rect((HEIGHT + 10), (HEIGHT / 3 * 2 + 30), (WIDTH - HEIGHT - 30), (HEIGHT / 3 - 15))
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+page_index = 0
 
 #INITIALIZE PYGAME
 pygame.init()
@@ -41,6 +42,8 @@ shop.set_shop_stock()
 stock = shop.get_shop_stock()
 
 def create_shop_pages(stock):
+
+    #CREATING SHOP PAGES THAT DISPLAY NO MORE THAN FIVE ITEMS AT A TIME
     global pages
     pages = []
     stock_index = 0
@@ -60,17 +63,14 @@ font = pygame.font.Font("freesansbold.ttf", 20)         #for shop items and pric
 smallfont = pygame.font.Font("freesansbold.ttf", 15)    #for BUY/SELL/NEXT/BACK buttons
 
 
-
 def display_shop_contents(page_index):
     current_page = pages[page_index]
     global line_spacing
     line_spacing = 25
-    button_spacing = 10
 
     #NEXT BUTTON TEXT INFO
     global nexttext
     nexttext = smallfont.render("NEXT", True, "#000000")
-    nexttextRect = nexttext.get_rect()
     global nexttextw 
     global nexttexth 
     nexttextw = nexttext.get_width()
@@ -79,7 +79,6 @@ def display_shop_contents(page_index):
     #BACK BUTTON TEXT INFO
     global backtext
     backtext = smallfont.render("BACK", True, "#000000")
-    backtextRect = backtext.get_rect()
     global backtextw 
     global backtexth 
     backtextw = backtext.get_width()
@@ -104,6 +103,9 @@ def display_shop_contents(page_index):
     WIN.blit(nexttext, ((WIDTH - nexttextw - 35), (HEIGHT - 20)))
     WIN.blit(backtext, ((HEIGHT + 25), (HEIGHT - 20)))
 
+    #CREATE LISTS TO STORE BUY AND SELL BUTTONS FOR EACH ITEM ON THE PAGE
+    global buy_buttons
+    global sell_buttons
     buy_buttons = []
     sell_buttons = []
 
@@ -112,7 +114,6 @@ def display_shop_contents(page_index):
         #SHOP PRICE TEXT INFO / CREATION
         global shoptext
         shoptext = font.render(f"{item}: ${item.get_cost()} / ${item.get_sell_price()}", True, "#000000")
-        shoptextRect = shoptext.get_rect()
         global shoptextw 
         global shoptexth 
         shoptextw = shoptext.get_width()
@@ -122,7 +123,6 @@ def display_shop_contents(page_index):
 
         #BUY BUTTON TEXT INFO
         buytext = smallfont.render("BUY", True, WHITE)
-        buytextRect = buytext.get_rect()
         global buytextw 
         global buytexth
         buytextw = buytext.get_width()
@@ -130,7 +130,6 @@ def display_shop_contents(page_index):
 
         #SELL BUTTON TEXT INFO
         selltext = smallfont.render("SELL", True, WHITE)
-        selltextRect = selltext.get_rect()
         global selltextw
         global selltexth
         selltextw = selltext.get_width()
@@ -158,34 +157,7 @@ def display_shop_contents(page_index):
         
         line_spacing += 30          #increment line spacing for items in shop (new item = new line)
 
-    global BUY0
-    global BUY1
-    global BUY2
-    global BUY3
-    global BUY4
-
-    global SELL0
-    global SELL1
-    global SELL2
-    global SELL3
-    global SELL4
-
-    #naming buy and sell buttons so they can be mapped to corresponding shop items
-    BUY0 = buy_buttons[0]
-    BUY1 = buy_buttons[1]
-    BUY2 = buy_buttons[2]
-    BUY3 = buy_buttons[3]
-    BUY4 = buy_buttons[4]
-
-    SELL0 = sell_buttons[0]
-    SELL1 = sell_buttons[1]
-    SELL2 = sell_buttons[2]
-    SELL3 = sell_buttons[3]
-    SELL4 = sell_buttons[4]
-
-    buy_buttons = [BUY0, BUY1, BUY2, BUY3, BUY4]
-    sell_buttons = [SELL0, SELL1, SELL2, SELL3, SELL4]
-
+    
 
 def clear_shop_page():
     #CREATE NEW SHOP WINDOW AND NEXT/BACK BUTTONS
@@ -194,7 +166,6 @@ def clear_shop_page():
     pygame.draw.rect(WIN, BLUE, BACK)
     WIN.blit(nexttext, ((WIDTH - nexttextw - 35), (HEIGHT - 20)))
     WIN.blit(backtext, ((HEIGHT + 25), (HEIGHT - 20)))
-
 
 
 class Game():
@@ -207,111 +178,109 @@ class Game():
         #pygame.draw.rect(WIN, BROWN, NEWS)
         pygame.draw.rect(WIN, BROWN, INVENTORY)
         pygame.draw.rect(WIN, BROWN, SHOP)
-
         create_shop_pages(stock)
-        display_shop_contents(0)
+        display_shop_contents(page_index)
         
     def click(self):
-        self.clicked = pygame.mouse.get_pressed()[0]    #i think this is the reason it works when you click
-        self.mouse_pos = pygame.mouse.get_pos()         #and not when you release, but i can't get anything
-        global page_index                               #else to work at all
-        page_index = 0
+        global page_index
+        self.mouse_pos = pygame.mouse.get_pos() 
 
-        if NEXT.collidepoint(self.mouse_pos) and self.clicked == True:      #advance one page in shop
+        if NEXT.collidepoint(self.mouse_pos):      #advance one page in shop
 
             if page_index < (len(pages) - 1):
                 page_index += 1
             
             elif page_index == (len(pages) - 1):
                 page_index = 0
-
             clear_shop_page()                           #erase current shop page
-            display_shop_contents(page_index)           #display new page
 
-        elif BACK.collidepoint(self.mouse_pos) and self.clicked == True:    #go back one page in shop
+
+        elif BACK.collidepoint(self.mouse_pos):    #go back one page in shop
 
             if page_index > 0:
                 page_index -= 1
             
             elif page_index == 0:
                 page_index = (len(pages) - 1)
-
             clear_shop_page()                           #erase current shop page
-            display_shop_contents(page_index)           #display new page
 
-        elif BUY0.collidepoint(self.mouse_pos) and self.clicked == True:    #it understands what item0 is and how
-            item0 = (pages[page_index])[0]                                  #much it costs, but actually buying it
-            player.buy(item0)                                               #doesn't work and idk why
+        elif buy_buttons[0].collidepoint(self.mouse_pos):    #it understands what item0 is and how
+            item0 = (pages[page_index])[0]                   #much it costs, but actually buying it
+            player.buy(item0)                                #doesn't work and idk why
             print(player.get_inventory())
             print(player.get_money())
 
-        elif BUY1.collidepoint(self.mouse_pos) and self.clicked == True:
-            player.buy((pages[page_index])[1])
+        elif (len(pages[page_index])) >= 2:
+            if buy_buttons[1].collidepoint(self.mouse_pos):
+                player.buy((pages[page_index])[1])
 
-        elif BUY2.collidepoint(self.mouse_pos) and self.clicked == True:
-            player.buy((pages[page_index])[2])
+        elif (len(pages[page_index])) >= 3:
+            if buy_buttons[2].collidepoint(self.mouse_pos):
+                player.buy((pages[page_index])[2])
 
-        elif BUY3.collidepoint(self.mouse_pos) and self.clicked == True:
-            player.buy((pages[page_index])[3])
+        elif (len(pages[page_index])) >= 4:
+            if buy_buttons[3].collidepoint(self.mouse_pos):
+                player.buy((pages[page_index])[3])
 
-        elif BUY4.collidepoint(self.mouse_pos) and self.clicked == True:
-            player.buy((pages[page_index])[4])
+        elif (len(pages[page_index])) >= 5:
+            if buy_buttons[4].collidepoint(self.mouse_pos):
+                player.buy((pages[page_index])[4])
 
-        elif SELL0.collidepoint(self.mouse_pos) and self.clicked == True:
+        elif sell_buttons[0].collidepoint(self.mouse_pos):
             player.sell((pages[page_index])[0])
 
-        elif SELL1.collidepoint(self.mouse_pos) and self.clicked == True:
-            player.sell((pages[page_index])[1])
+        elif (len(pages[page_index])) >= 2:
+          if sell_buttons[1].collidepoint(self.mouse_pos):
+                player.sell((pages[page_index])[1])
 
-        elif SELL2.collidepoint(self.mouse_pos) and self.clicked == True:
-            player.sell((pages[page_index])[2])
+        elif (len(pages[page_index])) >= 3:
+            if sell_buttons[2].collidepoint(self.mouse_pos):
+                player.sell((pages[page_index])[2])
 
-        elif SELL3.collidepoint(self.mouse_pos) and self.clicked == True:
-            player.sell((pages[page_index])[3])
+        elif (len(pages[page_index])) >= 4:
+            if sell_buttons[3].collidepoint(self.mouse_pos):
+                player.sell((pages[page_index])[3])
 
-        elif SELL4.collidepoint(self.mouse_pos) and self.clicked == True:
-            player.sell((pages[page_index])[4])
+        elif (len(pages[page_index])) >= 5:
+            if sell_buttons[4].collidepoint(self.mouse_pos):
+                player.sell((pages[page_index])[4])
 
-        pygame.display.update()
-
-                    
 
 
     def render(self):
         self.draw_window()
-        self.click()
         pygame.display.update()
 
-#WIN.fill(LIGHT_BROWN)
-
+#INITIALIZE GAME AND PLAYER
 game = Game()
 player = Player("playername")
 
 def main():
-    #Create Clock and run game
+    #CREATE CLOCK AND RUN GAME
     clock = pygame.time.Clock()
     run = True
+    create_shop_pages(stock)
+    display_shop_contents(page_index)
 
-    #runs the game
+    mousedown_last = False
+
     while run:
 
-        #makes it run at the desired frame rate
         clock.tick(FPS)
 
-        #closes game if window is closed
         for event in pygame.event.get():
 
+            #CLOSES GAME WINDOW
             if event.type == pygame.QUIT:
                 run = False
 
-            elif event.type == pygame.MOUSEBUTTONDOWN:  #neither MOUSEBUTTONDOWN or MOUSEBUTTONUP
-                game.click()                            #seem to do anything at all
-
-            elif event.type == pygame.MOUSEBUTTONUP:
-                game.click()
-
+        mousedown = pygame.mouse.get_pressed()[0]
+        if mousedown and not mousedown_last:
+            game.click()
         
-        #draw the screen every frame
+        mousedown_last = mousedown
+
+        #DRAW THE SCREEN EVERY FRAME
         game.render()
 
     pygame.quit()
